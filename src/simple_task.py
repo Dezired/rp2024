37c90bf850910af3ffc078223610222792ccdd8e
Dezired <philippherrmanns04@web.de>
2024-11-22 17:16:42 +0100

    uodated Paths

--- src/simple_task.py
+++ src/simple_task.py
@@ -8,7 +8,7 @@ from transform import Affine
 
 # setup
 RENDER = True
-URDF_PATH = "/home/jovyan/workspace/assets/urdf/robot.urdf"
+URDF_PATH = "/home/philipp/workspace/assets/urdf/robot.urdf"
 
 bullet_client = BulletClient(connection_mode=p.GUI)
 bullet_client.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
@@ -22,7 +22,7 @@ gripper = BulletGripper(bullet_client=bullet_client, robot_id=robot.robot_id)
 robot.home()
 
 # spawn an object
-object_urdf_path = "/home/jovyan/workspace/assets/objects/cube/object.urdf"
+object_urdf_path = "/home/philipp/workspace/assets/objects/cube/object.urdf"
 object_pose = Affine(translation=[0.5, 0, 0.1])
 object_id = bullet_client.loadURDF(
     object_urdf_path,
@@ -39,6 +39,14 @@ for _ in range(100):
 # keep in mind, that the object pose is defined in the world frame, and the eef points downwards
 # also, make sure that before grasping the gripper is open
 # consider adding a pre-grasp pose to ensure the object is grasped correctly without collision during approach
+
+
+position, quat = bullet_client.getBasePositionAndOrientation(object_id)
+object_pose = Affine(position, quat)
+
+# move over the object
+over_obj = Affine(translation=[0.5, 0, 0.5])
+robot.lin(over_obj)
 
 # close the simulation
 bullet_client.disconnect()
