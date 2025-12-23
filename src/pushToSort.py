39df6cf4bd305015cdbdee2fa9669aedbd459e0a
Dezired <philippherrmanns04@web.de>
2025-12-23 11:41:02 +0100

    update

--- src/pushToSort.py
+++ src/pushToSort.py
@@ -1,12 +1,22 @@
-from stable_baselines3 import PPO, DQN
+from stable_baselines3 import PPO, DQN, A2C
+from handleObjects import HandleObjects
+from handleEnvironment import HandleEnvironment
+from calcReward import CalcReward
 from sortingViaPushingEnv import sortingViaPushingEnv as svpEnv
 import numpy as np
+import yaml
 
-MODEL_PATH = 'data/models/80000'
-ENV = svpEnv()
+MODEL_PATH = '/home/philipp/Documents/repos/rp2024/data/models/A2C/80000'
+with open("src/config.yml", 'r') as stream:
+    config = yaml.safe_load(stream)
+
+hO = HandleObjects(config)
+hE = HandleEnvironment(config, hO)
+calcR = CalcReward(config, hE)
+ENV = svpEnv(config, hE, calcR)
 
 def test_model(model_path, env):
-    model = DQN.load(model_path)
+    model = A2C.load(model_path)
     obs, _info = env.reset()
     
     for _ in range(1000):
